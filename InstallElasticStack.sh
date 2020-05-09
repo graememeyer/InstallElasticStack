bc7dc58346f9573dfa5372fedf577a6e839a3781
Graeme Meyer <graememeyer@hotmail.com>
2020-05-09 17:12:17 +0100

    hasdhasd

--- InstallElasticStack.sh
+++ InstallElasticStack.sh
@@ -38,6 +38,19 @@ then echo "yes"
 else echo "no"; break;
 fi
 
+
+# Set the Kibana repository
+cat <<EOF | sudo tee /etc/yum.repos.d/elasticsearch.repo
+[kibana-7.x]
+name=Kibana repository for 7.x packages
+baseurl=https://artifacts.elastic.co/packages/7.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+EOF
+
 # Install Kibana from the RPM repository
 sudo dnf install -y kibana
 
